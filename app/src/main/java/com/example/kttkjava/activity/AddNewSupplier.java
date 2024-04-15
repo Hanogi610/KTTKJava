bfa7f14a502e0172bd99dca515c3fdf918401063
hanogi <hp6thoct@gmail.com>
2024-04-15 13:39:48 +0700

    a

--- app/src/main/java/com/example/kttkjava/activity/AddNewSupplier.java
+++ app/src/main/java/com/example/kttkjava/activity/AddNewSupplier.java
@@ -1,6 +1,9 @@
 package com.example.kttkjava.activity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +12,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.kttkjava.R;
+import com.example.kttkjava.controller.AppDatabase;
+import com.example.kttkjava.model.Supplier;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class AddNewSupplier extends AppCompatActivity {
 
+    private TextInputEditText addSupplierName, addSupplierPhone, addSupplierEmail, addSupplierAddress, addSupplierDescription;
+    private Button addSupplierButton, addCancelButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +30,53 @@ public class AddNewSupplier extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        init();
+        setup();
+    }
+
+    private void setup() {
+        addCancelButton.setOnClickListener(v -> {
+            finish();
+        });
+        addSupplierButton.setOnClickListener(v -> {
+            addSupplier();
+        });
+    }
+
+    private void init() {
+        addSupplierAddress = findViewById(R.id.add_supplier_address);
+        addSupplierDescription = findViewById(R.id.add_supplier_description);
+        addSupplierEmail = findViewById(R.id.add_supplier_email);
+        addSupplierName = findViewById(R.id.add_supplier_name);
+        addSupplierPhone = findViewById(R.id.add_supplier_phone);
+        addSupplierButton = findViewById(R.id.add_supplier_button);
+        addCancelButton = findViewById(R.id.add_cancel_button);
+    }
+
+    private void addSupplier() {
+        String name = addSupplierName.getText().toString();
+        String phone = addSupplierPhone.getText().toString();
+        String email = addSupplierEmail.getText().toString();
+        String address = addSupplierAddress.getText().toString();
+        String description = addSupplierDescription.getText().toString();
+        if (name.isEmpty() || phone.isEmpty() || email.isEmpty() || address.isEmpty() || description.isEmpty()) {
+            Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Supplier supplier = new Supplier(name, phone, email, address, description);
+        new AddSupplierTask().execute(supplier);
+    }
+    private class AddSupplierTask extends AsyncTask<Supplier, Void, Void> {
+        @Override
+        protected Void doInBackground(Supplier... suppliers) {
+            Supplier supplier = suppliers[0];
+            MainActivity.instance.supplierDAO().insert(supplier);
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            finish();
+        }
     }
 }
---
+++ app/src/main/java/com/example/kttkjava/adapter/ProductAdapter.java
@@ -0,0 +1,57 @@
+package com.example.kttkjava.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.kttkjava.R;
+import com.example.kttkjava.model.Product;
+
+import java.util.List;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
+    private List<Product> productList;
+    private Context context;
+
+    public ProductAdapter(List<Product> productList, Context context) {
+        this.productList = productList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.product_list_item, parent, false);
+        return new ProductViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
+        Product product = productList.get(position);
+        holder.productName.setText(product.getName());
+        holder.productPrice.setText((int) product.getBuyPrice());
+    }
+
+    @Override
+    public int getItemCount() {
+        return productList.size();
+    }
+
+    public void setProducts(List<Product> products) {
+        this.productList = products;
+        notifyDataSetChanged();
+}
+    class ProductViewHolder extends RecyclerView.ViewHolder{
+        TextView productName, productPrice;
+        public ProductViewHolder(@NonNull View itemView) {
+            super(itemView);
+            productName = itemView.findViewById(R.id.product_name);
+            productPrice = itemView.findViewById(R.id.product_price);
+        }
+    }
+}
